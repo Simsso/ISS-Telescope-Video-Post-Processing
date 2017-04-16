f0ac05205b1bd763c50cf5fd69339618141175d0
Timo Denk <github@timodenk.com>
2017-04-16 16:06:50 +0200

    Add processing code

---
+++ ISSTelescopeVideoPostProcessing/BitmapExtensions.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISSTelescopeVideoPostProcessing
+{
+    static class BitmapExtensions
+    {
+        public static float BrightnessOfBrightestPixels(this Bitmap bitmap, double pixelPercentage)
+        {
+            if (pixelPercentage < 0 || pixelPercentage > 1)
+            {
+                throw new ArgumentException("Pixel percentage must be in [0,1].");
+            }
+
+            int percentagePixelCount = (int)Math.Ceiling(bitmap.Width * bitmap.Height * pixelPercentage);
+
+            Color[] brightestPixels = new Color[percentagePixelCount];
+
+            int bufferIndex = 0;
+
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    Color pixel = bitmap.GetPixel(x, y);
+                    if (bufferIndex >= brightestPixels.Length)
+                    {
+                        Color darkest = DarkestColor(brightestPixels);
+                        for (int i = 0; i < brightestPixels.Length; i++)
+                        {
+                            if (brightestPixels[i] == darkest)
+                            {
+                                if (pixel.GetBrightness() > brightestPixels[i].GetBrightness())
+                                {
+                                    brightestPixels[i] = pixel;
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        brightestPixels[bufferIndex++] = pixel;
+                    }
+                }
+            }
+
+            float sum = 0;
+            for (int i = 0; i < brightestPixels.Length; i++)
+            {
+                sum += brightestPixels[i].GetBrightness();
+            }
+            return sum / brightestPixels.Length;
+        }
+
+        public static PointF GetBrightnessCenter(this Bitmap bitmap, float brightnessThreshold)
+        {
+            double xSum = 0, ySum = 0;
+            int count = 0;
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    if (bitmap.GetPixel(x, y).GetBrightness() >= brightnessThreshold)
+                    {
+                        xSum += x;
+                        ySum += y;
+                        count++;
+                    }
+                }
+            }
+
+            if (count == 0)
+            {
+                return new PointF(0, 0);
+            }
+            return new PointF((float)(xSum / count), (float)(ySum / count));
+        }
+
+        public static Bitmap Shift(this Bitmap bitmap, Point vec, Color fillColor)
+        {
+            Bitmap output = new Bitmap(bitmap.Width, bitmap.Height);
+            output.FillWithColor(fillColor);
+            for (int x = 0; x < bitmap.Width; x++)
+            {
+                for (int y = 0; y < bitmap.Height; y++)
+                {
+                    int targetX = vec.X + x,
+                        targetY = vec.Y + y;
+                    if (targetX >= 0 && targetX < bitmap.Width && targetY >= 0 && targetY < bitmap.Height)
+                    {
+                        output.SetPixel(targetX, targetY, bitmap.GetPixel(x, y));
+                    }
+                }
+            }
+            return output;
+        }
+
+        public static void FillWithColor(this Bitmap bitmap, Color color)
+        {
+            using (Graphics gfx = Graphics.FromImage(bitmap))
+            {
+                using (SolidBrush brush = new SolidBrush(color))
+                {
+                    gfx.FillRectangle(brush, 0, 0, bitmap.Width, bitmap.Height);
+                }
+            }
+        }
+
+        private static Color DarkestColor(Color[] array)
+        {
+            if (array.Length == 0)
+            {
+                throw new ArgumentException("Array must have length of at least 1");
+            }
+
+            Color darkest = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (darkest.GetBrightness() > array[i].GetBrightness())
+                {
+                    darkest = array[i];
+                }
+            }
+
+            return darkest;
+        }
+    }
+}
--- ISSTelescopeVideoPostProcessing/Program.cs
+++ ISSTelescopeVideoPostProcessing/Program.cs
@@ -1,15 +1,116 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Accord.Video.FFMPEG;
+using System;
+using System.Drawing;
+using System.IO;
 
 namespace ISSTelescopeVideoPostProcessing
 {
     class Program
     {
+        private static readonly float scalingFactor = 0.1f;
+
         static void Main(string[] args)
         {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("First argument must be an input path.");
+                Console.WriteLine("Second argument must be an output path.");
+                Console.ReadKey();
+                return;
+            }
+
+            string inputPath = args[0], outputPath = args[1];
+
+            FileAttributes fileAttributes = File.GetAttributes(inputPath);
+            if (fileAttributes.HasFlag(FileAttributes.Directory))
+            {
+                FrameCentering(inputPath, outputPath);
+            }
+            else
+            {
+                VideoProcessing(inputPath, outputPath);
+            }
+        }
+
+        private static void FrameCentering(string inputPath, string outputPath)
+        {
+            string[] files = Directory.GetFiles(inputPath);
+            for (int i = 0; i < files.Length; i++)
+            {
+                DrawTextProgressBar(i + 1, files.Length);
+                using (Bitmap image = new Bitmap(files[i]))
+                {
+                    PointF brightnessCenterVec = image.GetBrightnessCenter(0.1f),
+                        centerVec = new PointF(image.Width / 2, image.Height / 2);
+                    Point shiftVec = new Point((int)Math.Round(centerVec.X - brightnessCenterVec.X), (int)Math.Round(centerVec.Y - brightnessCenterVec.Y));
+                    using (Bitmap shifted = image.Shift(shiftVec, Color.FromArgb(0, 0, 0)))
+                    {
+                        shifted.Save(outputPath + @"\" + Path.GetFileName(files[i]));
+                    }
+                }
+            }
+
+            Console.WriteLine("Done");
+        }
+
+        private static void VideoProcessing(string videoPath, string outputFolder)
+        {
+            VideoFileReader videoFileReader = new VideoFileReader();
+            videoFileReader.Open(videoPath);
+            Console.WriteLine("Video loaded (" + videoFileReader.FrameCount + " frames)");
+
+            Size scaledSize = new Size((int)Math.Round(videoFileReader.Width * scalingFactor), (int)Math.Round(videoFileReader.Height * scalingFactor));
+
+            Console.WriteLine("Analyzing frames");
+
+            for (int i = 0; i < videoFileReader.FrameCount; i++)
+            {
+                DrawTextProgressBar(i + 1, (int)videoFileReader.FrameCount);
+                using (Bitmap frame = videoFileReader.ReadVideoFrame())
+                {
+                    using (Bitmap scaled = new Bitmap(frame, scaledSize))
+                    {
+                        float brightness = scaled.BrightnessOfBrightestPixels(0.02);
+                        if (brightness > 0.09f)
+                        {
+                            frame.Save(outputFolder + @"\" + i.ToString().PadLeft(5, '0') + ".bmp");
+                        }
+                    }
+                }
+            }
+            videoFileReader.Close();
+
+            Console.WriteLine("Done");
+        }
+
+
+        // http://stackoverflow.com/questions/24918768/progress-bar-in-console-application
+        private static void DrawTextProgressBar(int progress, int total)
+        {
+            // draw empty progress bar
+            float onechunk = 30.0f / total;
+
+            // draw filled part
+            int position = 0;
+            for (int i = 0; i < onechunk * progress; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.CursorLeft = position++;
+                Console.Write(" ");
+            }
+
+            // draw unfilled part
+            for (int i = position; i <= 31; i++)
+            {
+                Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.CursorLeft = position++;
+                Console.Write(" ");
+            }
+
+            // draw totals
+            Console.CursorLeft = 35;
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write(progress.ToString() + " of " + total.ToString() + "    "); //blanks at the end remove any excess
         }
     }
 }
